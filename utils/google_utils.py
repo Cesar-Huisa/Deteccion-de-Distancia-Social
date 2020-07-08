158624c8411fbe3ee57a2260bbfe1722c118b314
César Huisa <darkheart.x1@outlook.com>
2020-07-07 20:01:40 -0500

    rev 133c

--- utils/google_utils.py
+++ utils/google_utils.py
@@ -10,15 +10,15 @@ from pathlib import Path
 def attempt_download(weights):
     # Attempt to download pretrained weights if not found locally
     weights = weights.strip()
-    msg = weights + ' missing, try downloading from https://drive.google.com/drive/folders/1Drs_Aiu7xx6S-ix95f9kNsA6ueKRpN2J'
+    msg = weights + ' missing, try downloading from https://drive.google.com/file/d/14Y-rd6csihfs5igpnsRd3Vrg3vuBMztR/view?usp=sharing'
 
     r = 1
     if len(weights) > 0 and not os.path.isfile(weights):
-        d = {'yolov3-spp.pt': '1mM67oNw4fZoIOL1c8M3hHmj66d8e-ni_',  # yolov3-spp.yaml
-             'yolov5s.pt': '1R5T6rIyy3lLwgFXNms8whc-387H0tMQO',  # yolov5s.yaml
-             'yolov5m.pt': '1vobuEExpWQVpXExsJ2w-Mbf3HJjWkQJr',  # yolov5m.yaml
-             'yolov5l.pt': '1hrlqD1Wdei7UT4OgT785BEk1JwnSvNEV',  # yolov5l.yaml
-             'yolov5x.pt': '1mM8aZJlWTxOg7BZJvNUMrTnA2AbeCVzS',  # yolov5x.yaml
+        d = {'yolov3-spp.pt': '',  # yolov3-spp.yaml
+             'yolov5s.pt': '',  # yolov5s.yaml
+             'yolov5m.pt': '',  # yolov5m.yaml
+             'yolov5l.pt': '',  # yolov5l.yaml
+             'yolov5x.pt': '',  # yolov5x.yaml
              }
 
         file = Path(weights).name
